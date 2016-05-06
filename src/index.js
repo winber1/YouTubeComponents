38dc9637dcb8ff6d352aeb7acc5584c39ec2893b
winber1 <winber@cox.net>
2016-05-06 15:40:06 -0400

    finish section 1 of udemy.com/react-redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCpFFn5L-9JfU9-hOJpXPzCtWhE9AXhOAw';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// create a new componenet.  This component should produce HTML
+//  functional component; no state
+const App = () => {
+  return (
+          <div>
+              <SearchBar />
+          </div>
+        );
+}
+
+// take this component's generated HTML and put it on the page
+// - in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
