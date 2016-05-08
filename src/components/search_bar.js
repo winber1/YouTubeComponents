b1c9f1d44b89732232d2f676f6f9bfad38948d27
winber1 <winber@cox.net>
2016-05-08 09:18:18 -0400

    complete udemy ReduxSimpleStarter lesson 20

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,42 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Component } from 'react';
 
-/* functional component */
-/*
-const SearchBar = () => {
-  return <input />
-};
-*/
-
-/* refactor as...
-   class component  */
-/*
 class SearchBar extends Component {
-  render() {
-    return <input onChange={this.onInputChange} />;
-  }
-
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
-}
-*/
-class SearchBar extends Component {
-  // section 1 lesson 17
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    // put search term to state
     this.state = { term: '' };
   }
 
   render() {
     return (
       <div>
-        <input
-          value={this.state.term}
-          //onChange={event => this.setState({ term: event.target.value })}
-          />
-
+        <input onChange={event => this.setState({ term: event.target.value})} />
       </div>
     );
   }
